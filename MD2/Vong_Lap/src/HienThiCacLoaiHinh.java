9103c47fc0615415a16ca14fa228458ced038fd2
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-04 23:05:21 +0700

    HienThi20SNTDau

---
+++ MD2/Vong_Lap/src/HienThiCacLoaiHinh.java
@@ -0,0 +1,30 @@
+//import java.util.Scanner;
+//
+//public class HienThiCacLoaiHinh {
+//    public static void main(String[] args) {
+//        int height;
+//        int width;
+//        int choice;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Menu");
+//        System.out.println("1. Draw the triangle");
+//        System.out.println("2. Draw the square");
+//        System.out.println("3. Draw the rectangle");
+//        System.out.println("0. Exit");
+//        System.out.println("Enter your choice: ");
+//        choice = sc.nextInt();
+//
+//        switch (choice) {
+//            System.out.println("height: ");
+//            height = sc.nextInt();
+//            System.out.println("Width: ");
+//            width = sc.nextInt();
+//            for(int i = 1; i <= height; i++){
+//                for(int j = 1; j <= width; j++) {
+//                    System.out.print("*  ");
+//                }
+//                System.out.println("");
+//            }
+//        }
+//    }
+//}
