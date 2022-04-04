98e82335ae0e18dbde78ebcdffa007d2340e5b73
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-04 10:37:48 +0700

    ThucHanh_MD2_2

---
+++ MD2/src/ThucHanh_MD2_2.java
@@ -0,0 +1,16 @@
+public class ThucHanh_MD2_2 {
+    public static void main(String[] args) {
+        int i = 10;  //khai báo biến i  là kiểu số nguyên có giá trị là 10
+        float f = 20.5f; //khai báo biến f có kiểu là số thực có giá trị là 20.5
+        double d = 20.5; //khai báo biến f có kiểu là số thực lớn có giá trị là 20.5
+        boolean b = true; //khai báo biến b có kiểu logic có giá trị là true
+        char c =    'a'; //khai báo biến c có kiểu ký tự có giá trị là 'a'
+        String s = "Hà nội"; //khai báo biến s kiểu chuỗi có giá trị là "Hà nội"
+        System.out.println("i = "+ i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
