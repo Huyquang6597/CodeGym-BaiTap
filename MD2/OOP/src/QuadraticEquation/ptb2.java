665f9e51e625ce39daf1a249728b64d66f18018d
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-07 15:27:33 +0700

    Fan

---
+++ MD2/OOP/src/QuadraticEquation/ptb2.java
@@ -0,0 +1,33 @@
+package QuadraticEquation;
+
+public class ptb2 {
+    private double a ,b ,c;
+    private ptb2(){
+    }
+    public ptb2(double a , double b , double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double getA(){
+        return a;
+    }
+    public void setA(){
+        this.a = a;
+    }
+
+    public double getB(){
+        return b;
+    }
+
+    public void setB(){
+        this.b = b;
+    }
+    public double getC(){
+        return c;
+    }
+    public void setC(){
+        this.c = c;
+    }
+
+}
