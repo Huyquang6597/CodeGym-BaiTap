27c24fcf8cf38007824845ea7397d79289587ee0
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-26 09:47:15 +0700

    LuyenTapTextFile

--- MD2/TextFile/src/LuyenTapTextFile/LuyenTapFileWriterVaFileReader.java
+++ MD2/TextFile/src/LuyenTapTextFile/LuyenTapFileWriterVaFileReader.java
@@ -1,4 +1,46 @@
 package LuyenTapTextFile;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LuyenTapFileWriterVaFileReader {
+    public static void main(String[] args) throws IOException {
+        //Ghi ra file với FileWriter
+//        String txt = "Nguyen hihihi"; // Ghi chuỗi vào trong file
+//        String arr[] = {"Nhihih", "hdsjfhfwe", "uiqdfdf"};
+//
+//        try {
+//            FileWriter fw = new FileWriter("/Users/tranquanghuy/Desktop/CODEGYM - Bài tập/CodeGym-BaiTap/MD2/TextFile/src/LuyenTapTextFile/Demo"); //file writer mặc định sẽ tạo dữ liệu ới và xóa dữ liệu cũ đi
+//            BufferedWriter bw = new BufferedWriter(fw);
+//            for (String s : arr) {
+//                bw.write(s); //s là từng phần tử trong dãy arr[]
+//                bw.newLine(); //in xuống dòng
+//            }
+//            bw.write(txt);
+//            bw.close();
+//            fw.close();
+//
+//        } catch (Exception e) {
+//
+//        }
+
+        //Ghi ra file với FileWriter nhưng người dùng nhập vào rồi in ra file
+        Scanner scanner = new Scanner(System.in);
+        String txt = scanner.nextLine();
+        try {
+            FileWriter fw = new FileWriter("/Users/tranquanghuy/Desktop/CODEGYM - Bài tập/CodeGym-BaiTap/MD2/TextFile/src/LuyenTapTextFile/Demo");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(txt);
+            bw.close();
+            fw.close();
+
+        } catch (IOException e){
+
+        }
+
+
+    }
 }
--- MD2/TextFile/src/LuyenTapTextFile/TaoVaXoa.java
+++ MD2/TextFile/src/LuyenTapTextFile/TaoVaXoa.java
@@ -1,19 +1,32 @@
 package LuyenTapTextFile;
 
 import java.io.File;
+import java.io.IOException;
 
 public class TaoVaXoa {
-    public static void main(String[] args) {
-        try {
-            File file = new File("/Users/tranquanghuy/Desktop/CODEGYM - Bài tập/CodeGym-BaiTap/MD2/TextFile/src/LuyenTapTextFile/Demo");
-            if (file.delete()) {
-                System.out.println(file.getName() + "da xoa");
-            } else {
-                System.out.println("Xoa file khong thanh cong");
-            }
-        } catch (Exception e) {
-            System.out.println("Exception occurred");
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        //Tạo file
+        File file = new File("src/LuyenTapTextFile/hihi.txt");
+        if(!file.exists()){ //Kiểm tra file có tồn tại hay không
+            file.createNewFile(); //Tạo file mới
+        } else {
+            System.out.println("Da ton tai!"); //
         }
+
+
+
+
+        //Xóa file
+//        try {
+//            File file = new File("/Users/tranquanghuy/Desktop/CODEGYM - Bài tập/CodeGym-BaiTap/MD2/TextFile/src/LuyenTapTextFile/Demo");
+//            if (file.delete()) {
+//                System.out.println(file.getName() + "da xoa");
+//            } else {
+//                System.out.println("Xoa file khong thanh cong");
+//            }
+//        } catch (Exception e) {
+//            System.out.println("Exception occurred");
+//            e.printStackTrace();
+//        }
     }
 }
